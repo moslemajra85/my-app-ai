d501f90d0095664fc7206d79488ba129452a50f2
moslemajra85 <144964665+moslemajra85@users.noreply.github.com>
2025-11-22 13:36:24 +0100

    setup chadcn/ui

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Button } from "./components/ui/button";
 
 function App() {
   const [message, setMessage] = useState("");
@@ -14,7 +15,13 @@ function App() {
     fecthMessage();
   }, []);
 
-  return <p className="p-6 text-3xl">{message}</p>;
+  return (
+    <div className="p-4">
+      <p className="text-3xl">{message}</p>
+
+      <Button>Click Me</Button>
+    </div>
+  );
 }
 
 export default App;
