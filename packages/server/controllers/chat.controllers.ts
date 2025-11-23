7ee4c55194ff1bcafd4a3c5f33fd08db87c7629e
moslemajra85 <144964665+moslemajra85@users.noreply.github.com>
2025-11-23 13:07:38 +0100

    extract the logic inside the request handler into controller

---
+++ packages/server/controllers/chat.controllers.ts
@@ -0,0 +1,35 @@
+import type { Request, Response } from 'express';
+import { chatService } from '../services/chat.service';
+import z, { string } from 'zod';
+
+
+// implementation details
+const MessageSchema = z.object({
+   prompt: string()
+      .trim()
+      .min(1, 'Prompt is required')
+      .max(1000, "Prompt can't be longer than 1000 characters"),
+});
+
+//public interface
+export const chatController = {
+   sendMessage: async (req: Request, res: Response) => {
+      const parseResult = MessageSchema.safeParse(req.body);
+
+      if (!parseResult.success) {
+         res.status(400).json({ message: parseResult.error.format() });
+         return;
+      }
+
+      try {
+         const { prompt } = req.body;
+
+         const result = await chatService.sendMessage(prompt);
+         res.json({ message: result.response.text() });
+      } catch (error) {
+         res.status(500).json({
+            error: 'Failed to generetae a response.',
+         });
+      }
+   },
+};
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,12 +1,8 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import { GoogleGenerativeAI } from '@google/generative-ai';
-import z, { string } from 'zod';
-import { chatService } from './services/chat.service';
+import { chatController } from './controllers/chat.controllers';
 dotenv.config();
-
-const genAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
 
 const app = express();
 app.use(express.json());
@@ -23,32 +19,7 @@ app.get('/api/hello', (req: Request, res: Response) => {
    });
 });
 
-const MessageSchema = z.object({
-   prompt: string()
-      .trim()
-      .min(1, 'Prompt is required')
-      .max(1000, "Prompt can't be longer than 1000 characters"),
-});
-
-app.post('/api/chat', async (req: Request, res: Response) => {
-   const parseResult = MessageSchema.safeParse(req.body);
-
-   if (!parseResult.success) {
-      res.status(400).json({ message: parseResult.error.format() });
-      return;
-   }
-
-   try {
-      const { prompt } = req.body;
-
-      const result = await chatService.sendMessage(prompt);
-      res.json({ message: result.response.text() });
-   } catch (error) {
-      res.status(500).json({
-         error: 'Failed to generetae a response.',
-      });
-   }
-});
+app.post('/api/chat', chatController.sendMessage);
 
 app.listen(port, () => {
    console.log(`Server is running on port ${port}`);
