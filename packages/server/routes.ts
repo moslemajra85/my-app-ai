e8bc1ea08d4083df0751c2f006bc703dadc4589d
moslemajra85 <144964665+moslemajra85@users.noreply.github.com>
2025-11-23 13:15:17 +0100

    create route

---
+++ packages/server/routes.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+import { chatController } from './controllers/chat.controllers';
+
+const router = express.Router();
+
+router.get('/', (req: Request, res: Response) => {
+   res.send('Hello World!');
+});
+
+router.get('/api/hello', (req: Request, res: Response) => {
+   res.json({
+      message: 'Hello from the API!',
+   });
+});
+
+router.post('/api/chat', chatController.sendMessage);
+
+export default router;
+
