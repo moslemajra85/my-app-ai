bf5c970cb8f80679755f39f94adc11f15a14ead0
moslemajra85 <144964665+moslemajra85@users.noreply.github.com>
2025-11-29 18:25:00 +0100

    get a single product endpoint

--- packages/server/routes.ts
+++ packages/server/routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import { chatController } from './controllers/chat.controllers';
+import { prisma } from './prismaClient';
 
 const router = express.Router();
 
@@ -14,7 +15,23 @@ router.get('/api/hello', (req: Request, res: Response) => {
    });
 });
 
+router.get('/api/products/:id/reviews', async (req: Request, res: Response) => {
+   console.log('hello');
+   const productId = Number(req.params.id);
+   if(isNaN(productId)) return res.status(400).json({error: 'Invalid product ID'})
+   const reviews = await prisma.review.findMany({
+      where: {
+         productId,
+      },
+
+      orderBy: {
+         createdAt: 'desc',
+      },
+   });
+
+   console.log(reviews);
+   res.json(reviews);
+});
 router.post('/api/chat', chatController.sendMessage);
 
 export default router;
-
